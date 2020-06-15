df9a894857f0f3df5a8c6de39b25845cd28c58c7
phi dao <daovanphi25051994@gmail.com>
2020-06-15 15:37:06 +0700

    t
    creata dynamic message

--- src/main/java/com/codegym/controller/LoginController.java
+++ src/main/java/com/codegym/controller/LoginController.java
@@ -2,6 +2,9 @@ package com.codegym.controller;
 
 import com.codegym.model.Credential;
 import com.codegym.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cglib.core.Local;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,9 +13,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.Locale;
+
 @Controller
 @SessionAttributes("user")
 public class LoginController {
+
+    @Autowired
+    private MessageSource messageSource;
 
     @ModelAttribute("user")
     public User setUser(){
@@ -26,10 +34,11 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public ModelAndView login(@ModelAttribute("credential") Credential credential){
-        ModelAndView modelAndView = new ModelAndView("result");
-        User user = new User(credential.getUsername());
-        modelAndView.addObject("user", user);
+    public ModelAndView login(@ModelAttribute("credential") Credential credential, Locale locale){
+        ModelAndView modelAndView = new ModelAndView("login");
+        modelAndView.addObject("user", new User());
+        String message = messageSource.getMessage("login.message", null, locale);
+        modelAndView.addObject("message", message);
         return modelAndView;
 
     }
